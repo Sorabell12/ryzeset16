24eae274284c68e37cbe5e274a2118efe2dfd44c
Sorabell12 <tulinh0909245@gmail.com>
2025-11-22 08:59:58 +0700

    tft16

--- tft16.py
+++ tft16.py
@@ -196,7 +196,7 @@ def solve_three_strategies(pool, slots, user_emblems, prioritize_strength=False)
         
         has_galio = False
         final_team = list(team)
-        if traits.get("Demacia", 0) >= 5:
+        if traits.get("Demacia", 0) >= 6:
             has_galio = True
             final_team.append(GALIO_UNIT)
             tank_count += 1
@@ -412,3 +412,4 @@ if run:
 
 elif not run:
     st.info("👈 Select Level -> Click FIND TEAMS")
+
