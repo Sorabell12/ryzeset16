77d99675e28996a78a20ebe9bf4445269d5b7816
Sorabell12 <tulinh0909245@gmail.com>
2026-09-06 12:28:20 +0700

    Update tft16.py

--- tft16.py
+++ tft16.py
@@ -208,7 +208,7 @@ tab1, tab2 = st.tabs(["📷 Quét Camera", "📂 Tải ảnh lên"])
 img_file = None
 
 with tab1:
-    camera_input = st.camera_input("Chụp nhãn hàng (Đưa phần địa chỉ vào giữa)")
+    camera_input = st.camera_input("Chụp nhãn hàng (Đưa phần địa chỉ vào giữa)", facing_mode="environment")
     if camera_input:
         img_file = camera_input
 
